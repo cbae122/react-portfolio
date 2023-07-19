f745af4ddc23229b782c70f2b326e947a9fbb474
cbae <xchris.bae@gmail.com>
2023-07-19 13:43:16 -0700

    updated resume page

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,20 +3,28 @@ import React from 'react';
 export default function Home() {
   return (
     <div>
-      <h1>Resume</h1>
-      <hr />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
-      </p>
+      <div>
+        <h2>Resume</h2>
+        <hr />
+        <a href='resume' className='downloadbtn'>Download</a>
+        <span>
+
+        </span>
+        <span></span>
+      </div>
+      <br>
+      </br>
+      <div>
+        <h2>Skills</h2>
+        <hr />
+        <ul>
+          <li>HTML, CSS, Javascript</li>
+          <li>Web APIs, Third-Party APIs, Server-Side APIs</li>
+          <li>NodeJS, Object Oriented Programming, Express</li>
+          <li>SQL, ORM, MVC</li>
+          <li>NoSQL, PWA, React</li>
+        </ul>
+      </div>
     </div>
   );
 }

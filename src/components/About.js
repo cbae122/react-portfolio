29fc884cf6bed8b79a93f56e604c9b35023a5c77
cbae <xchris.bae@gmail.com>
2023-07-19 15:48:48 -0700

    updated about with picture

--- src/components/About.js
+++ src/components/About.js
@@ -1,22 +1,25 @@
 import React from 'react';
+import aboutImage from '../assets/projectImages/DSC00592.jpg';
 
 export default function About() {
   return (
-    <div>
+    <div className='aboutme'>
       <h1>Full Stack Developer</h1>
       <hr />
-      <img></img>
-      <p>
-        Chris Bae is currently a supply chain professional with a special focus in project management.
-        His background in demand and supply planning, inventory management and execution informs his mindful but competitive approach.
-      </p>
+      <div className='aboutimg'>
+        <img src={aboutImage} alt='profile'></img>
+        <p>
+          Chris Bae is currently a supply chain professional with a special focus in project management.
+          His background in demand and supply planning, inventory management and execution informs his mindful but competitive approach.
+        </p>
 
-      <p>
-        Chris is finally ready to persue his passion of computers and coding.
-        He has experience building computers.
-        Picking the parts, assembling, and installing software.
-        He is ready to learn the other side of the computer world which involves coding and software engineering.
-      </p>
+        <p>
+          Chris is finally ready to persue his passion of computers and coding.
+          He has experience building computers.
+          Picking the parts, assembling, and installing software.
+          He is ready to learn the other side of the computer world which involves coding and software engineering.
+        </p>
+      </div>
     </div>
   );
 }
